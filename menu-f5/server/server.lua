fd8a499f755f15784753909df5c4aa5ba618a3a1
DeaDTriX <97814611+DeaDTriX1@users.noreply.github.com>
2023-01-16 20:38:24 +0100

    Update server.lua

--- menu-f5/server/server.lua
+++ menu-f5/server/server.lua
@@ -239,3 +239,23 @@ AddEventHandler('menu-f5:sendtweet', function(playername, tweet)
     TriggerClientEvent('esx:showAdvancedNotification', -1, playername, "~b~New Tweet", tweet, "CHAR_ACTING_UP", 1)
 end)
 
+-- Factures
+ESX.RegisterServerCallback('menu-f5:facture', function(source, cb)
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	MySQL.Async.fetchAll('SELECT * FROM billing WHERE identifier = @identifier', {
+		['@identifier'] = xPlayer.identifier
+	}, function(result)
+		local bills = {}
+
+		for i = 1, #result do
+			bills[#bills + 1] = {
+				id = result[i].id,
+				label = result[i].label,
+				amount = result[i].amount
+			}
+		end
+
+		cb(bills)
+	end)
+end)
